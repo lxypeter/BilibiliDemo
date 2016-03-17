0e1983963806928e5ff86f2ef39e612676194a28
ztesoftIOS <lxypeter@xiyang.local>
2016-03-17 15:16:38 +0800

    bilibili内页效果实现

---
+++ BilibiliDemo/BilibiliDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BilibiliDemo
+//
+//  Created by Peter Lee on 16/3/16.
+//  Copyright © 2016年 Peter Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
