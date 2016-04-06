e7c42cc41c53efe71af5f5d533c0959be47e0260
CY.Lee <lxypeter@sina.com>
2016-04-06 17:16:51 +0800

    关注-标签页面实现

---
+++ BilibiliDemo/BilibiliDemo/Class/Focus/View/RoundTagButton.h
@@ -0,0 +1,15 @@
+//
+//  RoundTagButton.h
+//  BilibiliDemo
+//
+//  Created by Peter Lee on 16/4/5.
+//  Copyright © 2016年 Peter Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RoundTagButton : UIButton
+
++(RoundTagButton *)roundTagButtonWithTitle:(NSString *)title Color:(UIColor *)color Height:(CGFloat)height;
+
+@end
